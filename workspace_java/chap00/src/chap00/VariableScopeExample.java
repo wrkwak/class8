3f23f741fab18a46c4aca85e5725b608bbff70a7
곽우림 <dlwpekfms@gmail.com>
2024-11-25 17:17:55 +0900

    랜덤 함수

---
+++ workspace_java/chap00/src/chap00/VariableScopeExample.java
@@ -0,0 +1,10 @@
+package chap00;
+
+public class VariableScopeExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int v11 = (int)(Math.random()*10)%10;	
+		System.out.println(v11);
+		}
+	}
