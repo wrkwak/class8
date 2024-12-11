8239c7d4340bdc1b2db9ff3a7b49d7f93023aa27
곽우림 <dlwpekfms@gmail.com>
2024-12-11 15:44:28 +0900

    키오스크 만들기 v0.8

---
+++ workspace_java/chap06/src/sec04/exam03/Kimbob.java
@@ -0,0 +1,200 @@
+package sec04.exam03;
+
+import java.util.ArrayList;
+
+public class Kimbob {
+	   String name;    //지점명
+	   String address; //주소
+	   String cartM;
+	   String mn;
+	   String msg;
+	   
+	   //순서대로 저장함 arraylist
+	   ArrayList menu;  //메뉴들
+	   ArrayList price; //메뉴와 순서를 맞춰 관리하는 가격들
+	   
+	   ArrayList cartMenu;  //장바구니 메뉴들
+	   ArrayList cartPrice; //장바구니 가격
+	   ArrayList cartCnt; //장바구니 메뉴 수량
+	   
+	   ArrayList muNum;  //장바구니 메뉴 넘버
+	   ArrayList muNm; //장바구니 가격
+	   ArrayList mnPrice; //장바구니 메뉴 가격
+	   ArrayList mnCnt; //장바구니 메뉴 수량
+	   
+	   int seedMoney; //권리금
+	   int toSale=0;
+	   int cnt=0;
+	   int arrcnt=0;
+	   int totPrice =0;
+	   int totCnt =0;
+	   int tot = 0;
+	   int toSales=0;
+	   int num=0;
+	   int cartP;
+	   int cartC;
+	   int nm;
+	   int mp;
+	   int mc;
+	   int menuCnt=0;
+
+	   
+	   Kimbob(){ // 키오스크 가동시 동작
+	      this.menu = new ArrayList();
+	      this.price = new ArrayList();
+	      
+	      this.cartMenu = new ArrayList();
+	      this.cartPrice = new ArrayList();
+	      this.cartCnt = new ArrayList();
+	      
+	      this.muNum   = new ArrayList();    
+	      this.muNm    = new ArrayList();
+	      this.mnPrice = new ArrayList();
+	      this.mnCnt   = new ArrayList();
+	      
+	      this.menu.add("야채김밥");
+	      this.price.add(3500);
+	      
+	      this.menu.add("라면");
+	      this.price.add(4000);
+	   }
+	   
+	   Kimbob(int seedMoney){//시드머니 변경
+	      this.seedMoney = seedMoney;
+	      System.out.println("권리금이"+seedMoney+"원 으로 변경되었습니다.");
+	   }
+	   
+	   void store() {//매출값 누적
+	      toSales += tot;
+	   }
+	   
+	   void sales() {//매출값 출력
+		   System.out.print(toSales);
+	   }
+
+	   void menuAdd(String menu, int price) {//메뉴추가
+	      this.menu.add(menu);
+	      this.price.add(price);
+	      System.out.println("["+menu+","+price+"] 추가완료");
+	   }
+	   
+	   void priceMod(int menu, int price) {//가격변경
+		   this.price.set(menu,price);
+		   System.out.print("[");
+		   System.out.print(this.menu.get(menu));
+		   System.out.print(",");
+		   System.out.print(this.price.get(menu));
+		   System.out.println("] 변경완료!");
+	   }
+	   
+	   void order(int index, int cnt) {//주문하기
+	      String menuName = (String)this.menu.get(index); // 번호로 메뉴찾기
+	      int menuPrice = (int)this.price.get(index); // 번호로 메뉴찾기
+	      this.menuCnt = cnt;
+	      
+	      this.cartMenu.add(menuName);
+	      this.cartPrice.add(menuPrice);
+	      this.cartCnt.add(menuCnt); 
+	      
+	      System.out.println("주문이 장바구니에 담겼습니다.");
+	      System.out.println();
+	      this.arrcnt=1;
+	      cart();
+	      tot += totPrice;
+	   }
+
+	   
+	   void cart() {//장바구니
+		   if(arrcnt!=0) {
+			   for(int i = 0; i< this.arrcnt; i++) {
+				   int cartPrice = (int)this.cartPrice.get(i);
+				   totPrice = (cartPrice*menuCnt);
+				   
+				   cartM = (String)this.cartMenu.get(i); 
+				   cartP = (int)this.cartPrice.get(i); 
+				   cartC = (int)this.cartCnt.get(i); 
+				   
+				   cartStore(cartM,cartP,cartC);
+				   
+				   this.cartMenu.clear();
+				   this.cartPrice.clear();
+				   this.cartCnt.clear();
+				   
+				   msg ="장바구니 실행";
+			   }
+		   } else if(arrcnt == 0) {
+			   msg = "장바구니가 비었습니다.";
+			   System.out.println(msg);
+			   System.out.println();			   
+		   }
+	   }
+	   
+	   void cartStore(String muNm, int muPrice, int mnCnt) {//장바구니 메뉴 저장
+		   this.muNum.add(num+1);
+		   this.muNm.add(muNm);
+		   this.mnPrice.add(muPrice);
+		   this.mnCnt.add(mnCnt);
+		   num++;
+		   }
+	   
+	   void cartOut() {
+		   if(msg=="장바구니 실행") {
+			   for(int i = 0; i< num; i++) {
+				   nm = (int) this.muNum.get(i);
+				   mn = (String) this.muNm.get(i);
+				   mp = (int) this.mnPrice.get(i);
+				   mc = (int) this.mnCnt.get(i);			   
+				   System.out.println(nm+". "+mn+" "+mp+"원 수량 : "+mc);
+			   }
+			   System.out.println("총 금액 :"+tot+"원");
+		   } else {
+			   System.out.println("장바구니가 비었습니다.");
+		   }
+		   arrcnt = 0;
+	   }
+
+	   
+	   void chekIn() {//결제
+	      System.out.println("결제하실 총 금액 : "+this.tot+"원");
+	      System.out.println("결제완료 되었습니다.");
+	      System.out.println();
+	      
+	      this.cartMenu.clear();
+	      this.cartPrice.clear();
+	      this.cartCnt.clear();
+	      
+	      this.muNum.clear();
+	      this.muNm.clear();
+	      this.mnPrice.clear();
+	      this.mnCnt.clear();
+	      
+	      this.arrcnt = 0;
+	      this.store();
+	      this.tot=0;
+	      this.totPrice = 0;
+	      this.totCnt = 0;
+	      this.num = 0;
+	      
+	      msg = "장바구니가 비었습니다.";
+	   }
+	   
+	   void menuList() {//메뉴판
+	      for(int i = 0; i< this.menu.size(); i++) {
+	         System.out.print(i+" ");
+	         System.out.print(this.menu.get(i)+" - "); 
+	         System.out.println(this.price.get(i)+"원"); 
+	      }
+	   }
+
+//	   void print(int index, int count) {//영수증
+//	      String menuName = (String)this.menu.get(index);
+//	      int menuPrice = (int)this.price.get(index);
+//	      
+//	      System.out.println(":-:-:-:-:-:-:-:-:-:-:-:-:-:");
+//	      System.out.println("메뉴명 : "+menuName);
+//	      System.out.println("주문수 : "+count);
+//	      System.out.println("총금액 : "+menuPrice*count );
+//	      System.out.println(":-:-:-:-:-:-:-:-:-:-:-:-:-:");
+//	   }
+
+}
---
+++ workspace_java/chap06/src/sec04/exam03/KimbobExam.java
@@ -0,0 +1,104 @@
+package sec04.exam03;
+
+import java.util.Scanner;
+
+public class KimbobExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  Kimbob cheon = new Kimbob();
+	      Scanner scn = new Scanner(System.in);
+	      int admpwd = 2580;
+	      int pwdtryn = 0;
+	      int kiosk = 0;
+	      int kioskSm = 0;
+
+	      do {
+	         System.out.println("1.운영메뉴 2.관리자메뉴 3.종료");
+	         System.out.print("입력 : ");
+	         kiosk = scn.nextInt();
+
+	         if (kiosk == 2) {
+	            System.out.println("패스워드를 입력하세요.");
+	            int pwd = scn.nextInt();
+
+	            // 관리자 패스워드 확인(이것도 클래스로 바꿔야함)
+	            for (int i = 0; i <= 5; i++) {
+	               if (pwdtryn <= 5) {
+	                  if (pwd != admpwd) {
+	                     pwdtryn++;
+	                     System.out.println("비밀번호를 틀렸습니다.");
+	                     System.out.println("남은횟수 :" + (5 - pwdtryn) + "회");
+	                     System.out.println("패스워드를 입력하세요.");
+	                     pwd = scn.nextInt();
+	                     System.out.println();
+	                  } else if (pwd == admpwd) {
+	                     System.out.println("로그인 성공!");
+	                     System.out.println();
+	                     pwdtryn += 10;
+	                  }
+	               }
+	            }
+	            do {// 관리자 메뉴
+	               System.out.println("1.메뉴추가 2.가격변경 3.매출확인 4.종료");
+	               System.out.print("입력 : ");
+	               kiosk = scn.nextInt();
+
+	               if (kiosk == 1) {// 1.메뉴추가
+	                  if (kiosk == 1) {
+	                     System.out.println("추가하실 메뉴와 가격를 입력해주세요.");
+	                     System.out.print("추가할 메뉴 이름 :");
+	                     String menu = scn.next();
+	                     System.out.print("추가할 메뉴 가격 :");
+	                     int price = scn.nextInt();
+	                     cheon.menuAdd(menu, price);
+	                  }
+	               } else if (kiosk == 2) {// 2.가격변경
+	                  System.out.println("가격을 변경하실 메뉴의 번호를 선택해주세요.");
+	                  cheon.menuList();
+	                  int menuNum = scn.nextInt();
+	                  System.out.println("변경하실 가격을 입력해주세요.");
+	                  int price = scn.nextInt();
+	                  cheon.priceMod(menuNum, price);
+	                  System.out.println();
+	               } else if (kiosk == 3) {// 3.매출확인
+	                  System.out.print("오늘 매출 : ");
+	                  cheon.sales();
+	                  System.out.println("원");
+	                  System.out.println();
+	               }
+
+	            } while (!(kiosk == 4));
+	            
+
+	         } else if (kiosk == 1) {
+	            do {// 운영 메뉴
+	               System.out.println("1.주문 2.장바구니 확인 3.결제");
+	               System.out.print("입력 : ");
+	               System.out.println();
+	               
+	               if (kiosk == 1) {// 운영메뉴
+	                  kioskSm = scn.nextInt();
+	                  if (kioskSm == 1) {// 주문
+	                     System.out.println("주문하실 메뉴의 번호를 선택해주세요.");
+	                     cheon.menuList();
+	                     int menuNum = scn.nextInt();
+	                     
+	                     System.out.println("수량을 선택해주세요.");
+	                     int menuCnt = scn.nextInt();
+	                     cheon.order(menuNum,menuCnt);
+	                     
+	                  } else if (kioskSm == 2) {//장바구니 확인	                	 
+	                     cheon.cartOut();
+	                     
+	                  } else if (kioskSm == 3) {//결제
+	                     cheon.chekIn();
+	                  }
+	               }
+	            } while (!(kioskSm == 4));
+	         }
+	      } while (!(kiosk == 3));
+
+	}
+
+}
